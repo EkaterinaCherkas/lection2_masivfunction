48ccf741447178196a5528741363276dec41de10
Ekaterina Cherkas <ekaterina.cherkas12@mail.ru>
2023-02-28 00:04:41 +0300

    add task about max digit of the number

---
+++ practice2/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу ,которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа 
+Console.Clear();
+
+int num = new Random().Next(10, 100); // num = 16
+int a1 = num / 10 ; // 16 / 10 = 1
+int a2 = num % 10; // 16 % 10 = 6
+int max = a1;
+if (max < a2){
+    max = a2;
+}
+Console.WriteLine($"Max digit of the number {num} -> {max}");
+

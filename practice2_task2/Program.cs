87edb8764786e52adc4aeb8c539312bea28c8f78
Ekaterina Cherkas <ekaterina.cherkas12@mail.ru>
2023-03-01 17:33:00 +0300

    add two another methods of desicion

---
+++ practice2_task2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа. 456= 46 918=98
+Console.Clear();
+
+int num = new Random().Next(100, 1000);
+int a1 = num / 100;
+int a2 = num / 10;
+int a3 = num % 10;
+Console.WriteLine($"{num} -> {a1}{a3}");
+
+//two another methods
+//Console.Clear();
+//int num = new Random().Next(100, 1000);
+//int result = (num / 100) * 10 + num % 10;
+//Console.WriteLine($"{num} -> {result}");
+//Console.WriteLine($"{num} -> {num / 100}{num % 10}");

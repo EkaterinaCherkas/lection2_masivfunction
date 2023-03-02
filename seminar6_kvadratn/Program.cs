891485b6433e1fb8faa3baae41afa76c3301185e
Ekaterina Cherkas <ekaterina.cherkas12@mail.ru>
2023-03-02 16:58:21 +0300

    add task with таблица квадратов чисел от 1 до N

---
+++ seminar6_kvadratn/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+// 5 -> 1 4 9 16 25
+// 2 -> 1 4 
+
+Console.Clear();
+Console.WriteLine("Введите число");
+int N = int.Parse(Console.ReadLine());
+int i = 1;
+Console.Write($"{N} -> ");
+while (i <= N){
+    Console.Write($" {Math.Pow(i,2)}");
+    if (i != N){
+        Console.Write(", ");
+    }
+    i++;
+}
+// int i = 1 ;
+//string s = (i * i).Tostring() + (i != N ? ", " : "");
+

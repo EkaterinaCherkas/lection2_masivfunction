f14960b82647b9ca4af412af71999bdf8c13052a
Ekaterina Cherkas <ekaterina.cherkas12@mail.ru>
2023-03-03 01:15:00 +0300

    решение 1 задачи во 2 дз с помощью метода

---
+++ seminar8_function_task2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и показывает вторую цифру этого числа. 456= 5 918=1 782=8
+//Версия решения задачи из домашки с методами
+Console.Clear();
+
+int Prompt(string message){
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;}
+int number = Prompt("Введите трёхзначное число > ");
+if(number < 100 || number >= 1000){
+    Console.WriteLine("Вы ввели не трёхзначное число,пожалуйста повторите ввод");
+    return;}
+Console.WriteLine($"Введенное число {number} ");
+int seconddigit = number / 10 % 10;
+Console.WriteLine($"Вторая цифра {seconddigit} ");
+
